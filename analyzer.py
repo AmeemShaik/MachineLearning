8deeddc9c0173097f60ea998406cfb49c831a85f
AmeemShaik <shaik@live.unc.edu>
2014-03-11 21:35:03 +0100

    added sensitivity, precision, and specificity measurements

--- analyzer.py
+++ analyzer.py
@@ -8,54 +8,106 @@ import csv
 from sklearn.metrics import accuracy_score
 
 
-def analyzeNB(vectors,labels,kf,filename):
+def analyzeNB(vectors,labels,posLabel,kf,filename):
     bayesWriter = csv.writer(open(filename+".csv","wb"))
     gnb = GaussianNB()
-    total = 0
+    bayesWriter.writerow(["Iteration","Accuracy","Precision","Sensitivity","Specificity"])
+    total = {'accuracy':0,'precision':0,"sensitivity":0,"specificity":0}
     i = 0
     for train_index, test_index in kf:
         nb = gnb.fit(vectors[train_index],labels[train_index])
         nb_pred = nb.predict(vectors[test_index])
-        accuracy = accuracy_score(labels[test_index],nb_pred)
-        total+=accuracy
-        bayesWriter.writerow([i,accuracy])
+        metrics = __getMetrics(labels[test_index],nb_pred,posLabel)
+        accuracy = metrics['accuracy'] 
+        precision = metrics['precision']
+        sensitivity = metrics['sensitivity']
+        specificity = metrics['specificity']
+        total['accuracy']+=accuracy
+        total['precision']+=precision
+        total['sensitivity']+=sensitivity
+        total['specificity']+=specificity
+        bayesWriter.writerow([i,accuracy,precision,sensitivity,specificity])
         i+=1 
-    bayesWriter.writerow(["Average Accuracy",total/len(kf)])
+    bayesWriter.writerow(["Averages",total['accuracy']/len(kf),total['precision']/len(kf),total['sensitivity']/len(kf),total['specificity']/len(kf)])
 
 
 
-def analyzeRF(vectors,labels,kf,maxTrees,maxFeatures,filename):
+def analyzeRF(vectors,labels,posLabel,kf,maxTrees,maxFeatures,filename):
     rfWriter = csv.writer(open(filename+".csv","wb"))
-    rfWriter.writerow(["Number of trees", "Average accuracy"])
+    rfWriter.writerow(["Number of trees", "Average accuracy","Average Precision","Average Sensitivity","Average Specificity"])
     for numTrees in range(1,maxTrees+1):
         randomForest = RandomForestClassifier(n_estimators=numTrees)
-        rf_total = 0
+        total = {'accuracy':0,'precision':0,"sensitivity":0,"specificity":0}
         for train_index, test_index in kf:
             rf = randomForest.fit(vectors[train_index],labels[train_index])
             rf_pred = rf.predict(vectors[test_index])
-            rf_total+=accuracy_score(labels[test_index],rf_pred)
+            metrics = __getMetrics(labels[test_index],rf_pred,posLabel)
+            accuracy = metrics['accuracy'] 
+            precision = metrics['precision']
+            sensitivity = metrics['sensitivity']
+            specificity = metrics['specificity']
+            total['accuracy']+=accuracy
+            total['precision']+=precision
+            total['sensitivity']+=sensitivity
+            total['specificity']+=specificity
+        rfWriter.writerow([numTrees,total['accuracy']/len(kf),total['precision']/len(kf),total['sensitivity']/len(kf),total['specificity']/len(kf)])
 
-        rfWriter.writerow([numTrees,rf_total/len(kf)]) 
-
-    rfWriter.writerow(["Number of features","Average accuracy"])
     for features in range(1,maxFeatures+1):
         randomForest = RandomForestClassifier(n_estimators=30,max_features=features)
-        total = 0
+        rfWriter.writerow(["Number of features","Average accuracy","Average Precision","Average Sensitivity", "Average Specificity"])
+        total = {'accuracy':0,'precision':0,"sensitivity":0,"specificity":0}
         for train_index, test_index in kf:
             rf = randomForest.fit(vectors[train_index],labels[train_index])
             rf_pred = rf.predict(vectors[test_index])
-            total+=accuracy_score(labels[test_index],rf_pred)
-        rfWriter.writerow([features,total/len(kf)])
+            metrics = __getMetrics(labels[test_index],rf_pred,posLabel)
+            accuracy = metrics['accuracy'] 
+            precision = metrics['precision']
+            sensitivity = metrics['sensitivity']
+            specificity = metrics['specificity']
+            total['accuracy']+=accuracy
+            total['precision']+=precision
+            total['sensitivity']+=sensitivity
+            total['specificity']+=specificity
+        rfWriter.writerow([features,total['accuracy']/len(kf),total['precision']/len(kf),total['sensitivity']/len(kf),total['specificity']/len(kf)])
+
          
-         
-def analyzeKNN(vectors,labels,kf,maxNeighbors,filename):
+def analyzeKNN(vectors,labels,posLabel,kf,maxNeighbors,filename):
     knnWriter = csv.writer(open(filename+".csv","wb"))
-    knnWriter.writerow(["Number of neighbors","Average accuracy"])
+    knnWriter.writerow(["Number of neighbors","Average accuracy","Average Precision","Average Sensitivity", "Average Specificity"])
     for numNeighbors in range(1,maxNeighbors+1):
         knn = KNeighborsClassifier(n_neighbors=numNeighbors)
-        total = 0
+        total = {'accuracy':0,'precision':0,"sensitivity":0,"specificity":0}
         for train_index, test_index in kf:
             neigh = knn.fit(vectors[train_index],labels[train_index])
             neigh_pred = neigh.predict(vectors[test_index])
-            total+=accuracy_score(labels[test_index],neigh_pred)
-        knnWriter.writerow([numNeighbors, total/len(kf)])
+            metrics = __getMetrics(labels[test_index],neigh_pred,posLabel)
+            accuracy = metrics['accuracy'] 
+            precision = metrics['precision']
+            sensitivity = metrics['sensitivity']
+            specificity = metrics['specificity']
+            total['accuracy']+=accuracy
+            total['precision']+=precision
+            total['sensitivity']+=sensitivity
+            total['specificity']+=specificity
+        knnWriter.writerow([numNeighbors,total['accuracy']/len(kf),total['precision']/len(kf),total['sensitivity']/len(kf),total['specificity']/len(kf)])
+        
+
+def __getMetrics(trueLabels,predLabels,posLabel):
+    truePos=trueNeg=falsePos=falseNeg = 0
+    for i in range(len(trueLabels)):
+       if(trueLabels[i]==posLabel):
+           if(predLabels[i]==posLabel):
+               truePos+=1
+           else:
+               falseNeg+=1
+       else:
+           if(predLabels[i]==posLabel):
+               falsePos+=1
+           else:
+               trueNeg+=1
+    results={}
+    results['accuracy']=(truePos+trueNeg)/float(truePos+falsePos+falseNeg+trueNeg)
+    results['precision']=(truePos)/float(truePos+falsePos)
+    results['sensitivity'] =(truePos)/float(truePos+falseNeg)
+    results['specificity'] =(trueNeg)/float(trueNeg+falsePos)
+    return results 
--- collectData.py
+++ collectData.py
@@ -6,7 +6,7 @@ from sklearn.cross_validation import KFold
 labels = np.loadtxt('tumorData.csv',delimiter=',',dtype='|S6',usecols=(0,))
 vectors = np.loadtxt('tumorData.csv',delimiter=',',usecols=(range(1,31)))
 kf = KFold(len(labels),n_folds=10,indices = False,shuffle=True,random_state=42)
-analyzer.analyzeNB(vectors,labels,kf,"TumorNBResults")
-analyzer.analyzeRF(vectors,labels,kf,100,30,"TumorRFResults")
-analyzer.analyzeKNN(vectors,labels,kf,100,"TumorKNNResults")
+analyzer.analyzeNB(vectors,labels,'M',kf,"TumorNBResults")
+analyzer.analyzeRF(vectors,labels,'M',kf,100,30,"TumorRFResults")
+analyzer.analyzeKNN(vectors,labels,'M',kf,100,"TumorKNNResults")
 
